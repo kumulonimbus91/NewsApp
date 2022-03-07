72e68c5c065ffb108323b722a76932fcfc02f236
kumulonimbus91 <69019613+kumulonimbus91@users.noreply.github.com>
2022-03-07 01:22:52 +0100

    Initial commit

---
+++ app/src/main/java/com/nenad/newsapp/UI/activities/IntroScreen.kt
@@ -0,0 +1,12 @@
+package com.nenad.newsapp.UI.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nenad.newsapp.R
+
+class IntroScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro_screen)
+    }
+}
---
+++ app/src/main/java/com/nenad/newsapp/UI/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.nenad.newsapp.UI.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nenad.newsapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
