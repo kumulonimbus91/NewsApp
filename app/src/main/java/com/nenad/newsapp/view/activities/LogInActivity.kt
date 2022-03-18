7b917a45b5a84daac93fb3b997e016bcfd02c8f7
kumulonimbus91 <69019613+kumulonimbus91@users.noreply.github.com>
2022-03-18 15:22:11 +0100

    remote data source

--- app/src/main/java/com/nenad/newsapp/view/activities/LogInActivity.kt
+++ app/src/main/java/com/nenad/newsapp/view/activities/LogInActivity.kt
@@ -1,12 +1,9 @@
 package com.nenad.newsapp.view.activities
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Toast
-import androidx.core.content.ContextCompat
-import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
