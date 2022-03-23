be32b3ae14001753644f21c716f1d077bb3702e7
kumulonimbus91 <69019613+kumulonimbus91@users.noreply.github.com>
2022-03-23 20:00:42 +0100

    adapter, module

--- app/src/main/java/com/nenad/newsapp/view/activities/MainActivity.kt
+++ app/src/main/java/com/nenad/newsapp/view/activities/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var navHostFragment: NavHostFragment
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMainBinding.inflate(layoutInflater)
