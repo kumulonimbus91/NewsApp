30116eb18e33978282e97c94921df7752ffe8247
kumulonimbus91 <69019613+kumulonimbus91@users.noreply.github.com>
2022-04-26 15:50:30 +0200

    search fragment, room db

--- app/src/main/java/com/nenad/newsapp/view/activities/MainActivity.kt
+++ app/src/main/java/com/nenad/newsapp/view/activities/MainActivity.kt
@@ -1,14 +1,13 @@
 package com.nenad.newsapp.view.activities
 
-import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
@@ -28,10 +27,21 @@ class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var actionBar: ActionBar
 
+    lateinit var viewModel: MainViewModel
+
+
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMainBinding.inflate(layoutInflater)
+
+       // mBinding.viewModel = viewModel
+
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
 
         //  supportActionBar?.hide()
         actionBar = supportActionBar!!
@@ -70,6 +80,8 @@ class MainActivity : AppCompatActivity() {
 
 
 
+
+
         setContentView(mBinding.root)
     }
 
@@ -82,59 +94,6 @@ class MainActivity : AppCompatActivity() {
     private fun drawerItemSelectedListener() {
         mBinding.navView.setNavigationItemSelectedListener {
             when (it.itemId) {
-                R.id.business -> {
-                    saveSp("category", "business")
-
-
-
-                    if (mBinding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
-                        mBinding.drawerLayout.closeDrawer(GravityCompat.START)
-                    }
-
-                    return@setNavigationItemSelectedListener true
-                }
-                R.id.technology -> {
-                    saveSp("category", "technology")
-                    if (mBinding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
-                        mBinding.drawerLayout.closeDrawer(GravityCompat.START)
-                    }
-
-                    return@setNavigationItemSelectedListener true
-                }
-                R.id.science -> {
-                    saveSp("category", "science")
-                    if (mBinding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
-                        mBinding.drawerLayout.closeDrawer(GravityCompat.START)
-                    }
-
-                    return@setNavigationItemSelectedListener true
-
-                }
-                R.id.health -> {
-                    saveSp("category", "health")
-                    if (mBinding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
-                        mBinding.drawerLayout.closeDrawer(GravityCompat.START)
-                    }
-
-                    return@setNavigationItemSelectedListener true
-
-                }
-                R.id.entertainment -> {
-                    saveSp("category", "entertainment")
-                    if (mBinding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
-                        mBinding.drawerLayout.closeDrawer(GravityCompat.START)
-                    }
-
-                    return@setNavigationItemSelectedListener true
-                }
-                R.id.sport -> {
-                    saveSp("category", "sports")
-                    if (mBinding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
-                        mBinding.drawerLayout.closeDrawer(GravityCompat.START)
-                    }
-
-                    return@setNavigationItemSelectedListener true
-                }
                 R.id.settings -> {
 
                     val intent = Intent(this@MainActivity, SettingsActivity::class.java)
@@ -143,6 +102,13 @@ class MainActivity : AppCompatActivity() {
 
 
                     return@setNavigationItemSelectedListener true
+                }
+                R.id.nav_sign_out -> {
+
+
+
+                    return@setNavigationItemSelectedListener true
+
                 }
 
 
@@ -156,11 +122,7 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun saveSp(key: String, value: String) {
-        PreferenceManager.getDefaultSharedPreferences(this)
-            .edit()
-            .putString(key, value).apply()
-    }
+
 }
 
 
